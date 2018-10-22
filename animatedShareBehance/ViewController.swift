a29917e8274a97e3501ccfd6ec28b07b89daf5db
gme_2 <gme_2@gme-2s-imac.local>
2018-10-22 10:54:50 +0900

    Initial Commit

---
+++ animatedShareBehance/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  animatedShareBehance
+//
+//  Created by gme_2 on 22/10/2018.
+//  Copyright © 2018 gme_2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
